6b0bcffe0deb64fe20edc0209afeef960d2acd79
Json Zhou <zzhou292@wisc.edu>
2025-08-26 16:07:07 -0500

    [08-26-2025-16-07] migration to bazel system

--- compile.sh
+++
@@ -1,6 +0,0 @@
-nvcc test.cu GPUMemoryManager.cu -o test \
-  -I. -I/usr/include/eigen3 -O3 \
-  -gencode arch=compute_61,code=sm_61 \
-  -gencode arch=compute_75,code=sm_75 \
-  -gencode arch=compute_86,code=sm_86 \
-  -gencode arch=compute_86,code=compute_86   # PTX fallback (forward-compat)
