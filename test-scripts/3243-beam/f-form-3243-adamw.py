corrected for gradient error in alm formulation

test-scripts/3243-beam/f-form-3243-adamw.py
@@ -576,7 +576,7 @@ for step in range(Nt):
 
                 J = constraint_jacobian(qA)
                 constr_violation = constraint(qA)
-                return g + J.T @ (lam + rho * h * constr_violation)
+                return g + h*(J.T @ (lam + rho * constr_violation))
 
             # === AdamW parameters ===
             lr = 2e-4
@@ -618,7 +618,7 @@ for step in range(Nt):
             v = v_current
             qA = q_prev + h * v
             constr_violation = constraint(qA)
-            lam += rho * h * constr_violation
+            lam += rho * constr_violation
 
             print(
                 f">>>>> End of OUTER STEP #{outer_iter}; ||c|| = {np.linalg.norm(constr_violation):.2e}")

test-scripts/T10-tets/f-form-T10-nesterov.py
@@ -401,7 +401,7 @@ if __name__ == "__main__":
                 g_mech = (M @ (v_loc - v_prev)) / h - (-f_int_dyn.flatten() + f_ext)
                 J = constraint_jacobian(qA)
                 cA = constraint(qA)
-                return g_mech + J.T @ (lam + rho_bb*h*cA)
+                return g_mech + h*(J.T @ (lam + rho_bb*cA))
 
             v_k   = v.copy()
             v_km1 = v.copy()
@@ -432,7 +432,7 @@ if __name__ == "__main__":
             v = v_k
             qA = q_prev + h*v
             cA = constraint(qA)
-            lam += rho_bb*h*cA
+            lam += rho_bb*cA
             print(f">>>>> OUTER {outer_iter}: ||c||={np.linalg.norm(cA):.3e}")
 
         return v, lam